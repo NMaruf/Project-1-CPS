8c80fb7af4b0779f93dc1821f9706bf82ca61602
Maruf Nodirov <nodirov00@list.ru>
2022-04-28 01:04:05 +0300

    Блок 6

--- script.js
+++ script.js
@@ -1,19 +1,42 @@
-new Swiper('.swiper',{
-    direction: 'horizontal',
-    loop: false,
-    spaceBetween: 16,
-    slidesPerView: 'auto',
-    mousewheel: true,
-    slidesOffsetBefore: 16,
-    slidesOffsetAfter: 16,
+const slider = document.querySelector('.swiper-container');
 
-     navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev'
-    },
+let mySwiper;
 
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true
-    },
+function mobileSlider() {
+    if (window.innerWidth < 768 && slider.dataset.mobile === 'false') {
+        mySwiper = new Swiper('.swiper',{
+            direction: 'horizontal',
+            loop: false,
+            spaceBetween: 16,
+            slidesPerView: 'auto',
+            mousewheel: true,
+            slidesOffsetBefore: 16,
+            slidesOffsetAfter: 16,
+
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev'
+            },
+
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true
+            },
+        })
+        slider.dataset.mobile = 'true'
+    }
+
+    if (window.innerWidth >= 768) {
+        slider.dataset.mobile = 'false';
+        if (slider.classList.contains('.swiper-container-initialized')) {
+            mySwiper.destroy();
+        }
+    }
+}
+
+mobileSlider();
+
+window.addEventListener('resize', () => {
+    mobileSlider();
 })
+
