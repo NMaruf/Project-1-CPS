f7ffaebaa13884b1b30e82e3d958fd05d17da1d7
Maruf Nodirov <nodirov00@list.ru>
2022-05-11 00:55:30 +0300

    Внесены правки в блок 6

--- script.js
+++ script.js
@@ -1,42 +1,13 @@
-const slider = document.querySelector('.swiper-container');
-
-let mySwiper;
-
-function mobileSlider() {
-    if (window.innerWidth < 768 && slider.dataset.mobile === 'false') {
-        mySwiper = new Swiper('.swiper',{
-            direction: 'horizontal',
-            loop: false,
-            spaceBetween: 16,
-            slidesPerView: 'auto',
-            mousewheel: true,
-            slidesOffsetBefore: 16,
-            slidesOffsetAfter: 16,
-
-            navigation: {
-                nextEl: '.swiper-button-next',
-                prevEl: '.swiper-button-prev'
-            },
-
-            pagination: {
-                el: '.swiper-pagination',
-                clickable: true
-            },
-        })
-        slider.dataset.mobile = 'true'
-    }
-
-    if (window.innerWidth >= 768) {
-        slider.dataset.mobile = 'false';
-        if (slider.classList.contains('.swiper-container-initialized')) {
-            mySwiper.destroy();
+let showSlides = function () {
+    let hiddenSlides = document.querySelectorAll('.hidden');
+    let buttonShow = document.querySelector('.show');
+    for (let i = 0; i <= hiddenSlides.length; i++) {
+        if (hiddenSlides[i].style.display === 'none') {
+            hiddenSlides[i].style.display = 'flex';
+            buttonShow.style.display = 'none'
+        } else {
+            hiddenSlides[i].style.display = 'none';
+            buttonShow.style.display = 'block'
         }
     }
-}
-
-mobileSlider();
-
-window.addEventListener('resize', () => {
-    mobileSlider();
-})
-
+};
